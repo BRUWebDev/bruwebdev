8d61865521fb0850a337e37a069bf6e25c4f9a3c
Nick Brunner <njbrunner@gmail.com>
2025-07-26 12:42:10 -0600

    Fixed linting issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 function openPortfolioModal(url) {
-  const portfolioModal = new bootstrap.Modal('#modal-portfolio', {
-    keyboard:false
+  const portfolioModal = new bootstrap.Modal("#modal-portfolio", {
+    keyboard: false,
   });
   const portfolioPreview = document.getElementById("portfolio-preview");
   portfolioPreview.setAttribute("src", url);
